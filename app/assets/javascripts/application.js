7616db1573a5f945e3396deda4cc03cae6fc43d8
Brice <bgatelet@gmail.com>
2015-06-29 17:16:49 -0400

    modify asset pipeline

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,9 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.turbolinks
 //= require jquery_ujs
 //= require bootstrap
-//= require jquery.turbolinks
 //= require turbolinks
+//= require polling
 //= require_tree .
